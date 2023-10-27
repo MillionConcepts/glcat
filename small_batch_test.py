a978566f2f186a626244b98554b4224ff05b9d7b
[bekahalbach] <[bekah.albach@gmail.com]>
2023-10-27 10:13:33 -0700

    whoops can't iterate something that doesn't exist

--- small_batch_test.py
+++ small_batch_test.py
@@ -1,12 +1,12 @@
 from run_correct_on_ec2 import test_eclipse
 
-e = [580, 1436, 1519, 1696, 1687, 1710, 1752, 1817, 1777, 6411,
+eclipses = [580, 1436, 1519, 1696, 1687, 1710, 1752, 1817, 1777, 6411,
      5267, 7535, 1819, 6268, 6411, 6413, 10507, 11604, 15466,
      23725, 25191, 25192, 26418, 26420, 1414, 1420, 842, 769,
      589, 7581, 9010]
 
 try:
-    for e in list:
+    for e in eclipses:
         print(f"Testing eclipse {e}")
         test_eclipse(e)
 finally:
