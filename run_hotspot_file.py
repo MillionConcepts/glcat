4b00fa638657dcb6b2b06d32e8d0ff42bb758c75
[bekahalbach] <[bekah.albach@gmail.com]>
2024-04-30 22:40:32 -0700

    list of eclipses plus iterable

--- run_hotspot_file.py
+++ run_hotspot_file.py
@@ -1,7 +1,16 @@
 if __name__ == "__main__":
     from backplanes import make_hotspot_files_eclipse
+    import pandas as pd
 
-    make_hotspot_files_eclipse(
-        eclipse=580,
-        band="NUV"
-    )
+    nuv_list = pd.read_csv("every3_nuv.csv")
+
+    # Access the column as a pandas Series object
+    eclipses = nuv_list['eclipse']
+
+    # Now, you can iterate through the values in the column_series
+    for e in eclipses:
+        make_hotspot_files_eclipse(
+            eclipse=e,
+            band="NUV"
+        )
+
