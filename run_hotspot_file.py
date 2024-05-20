2fcb30498757135ffd09391efac29eb689516033
[bekahalbach] <[bekah.albach@gmail.com]>
2024-05-20 11:29:13 -0700

    nuv no ngs list

--- run_hotspot_file.py
+++ run_hotspot_file.py
@@ -2,17 +2,17 @@ if __name__ == "__main__":
     from backplanes import make_hotspot_files_eclipse
     import pandas as pd
     import gc
-    nuv_list = pd.read_csv("every3_fuv.csv")
+    nuv_list = pd.read_csv("nuv_no_ngs.csv")
 
     # Access the column as a pandas Series object
-    eclipses = nuv_list['eclipse'][4458:]
+    eclipses = nuv_list['eclipse'][450:]
 
     # Now, you can iterate through the values in the column_series
     for e in eclipses:
         e = int(e)
         make_hotspot_files_eclipse(
             eclipse=e,
-            band="FUV"
+            band="NUV"
         )
         gc.collect()
 
