82a2e9947291f300261d057a0ea808c3de57fd3d
[bekahalbach] <[bekah.albach@gmail.com]>
2024-08-23 10:38:11 -0700

    add edge points for mask continuity

--- backplanes/make_masks.py
+++ backplanes/make_masks.py
@@ -10,7 +10,7 @@ import pyarrow.parquet as parquet
 
 def make_masks_per_eclipse(eclipse, band, photonlist_path, nbins, savepath):
     # use existing photonlists to make individual hotspot and coldspot masks
-    # per eclipse band
+    # per eclipse & band
 
     photon_file = str(savepath + photonlist_path)
     print(photon_file)
@@ -18,7 +18,6 @@ def make_masks_per_eclipse(eclipse, band, photonlist_path, nbins, savepath):
         try:
             # get photonlist from bucket
             print("reading photonlist")
-
             photonlist = parquet.ParquetFile(photon_file)
             if photonlist.metadata.num_rows < 40000000: # kind of an arbitrary cutoff
 
@@ -29,10 +28,10 @@ def make_masks_per_eclipse(eclipse, band, photonlist_path, nbins, savepath):
                 nf['col_rnd'] = nf['col'].round().astype(int)
 
                 print("filtering photonlist")
-                # filtering photonlist for on detector
-                nf = nf[(nf['col_rnd'] < 800) & (nf['row_rnd'] < 800)
+                # filtering photonlist for on detector because read_row_groups can't
+                nf = nf[(nf['col_rnd'] <= 800) & (nf['row_rnd'] <= 800)
                         & (nf['ra'] != 0) & (nf['dec'] != 0)
-                        & (nf['col_rnd'] >= -50) & (nf['row_rnd'] >= -50)]
+                        & (nf['col_rnd'] >= 0) & (nf['row_rnd'] >= 0)]
                 mask = pd.notna(nf['ra'])
                 nf = nf[mask]
 
@@ -40,34 +39,41 @@ def make_masks_per_eclipse(eclipse, band, photonlist_path, nbins, savepath):
                 # rough approx not accounting for dead time
                 expt = nf.iloc[len(nf) - 1]['t'] - nf.iloc[0]['t']
 
+                ra = nf.iloc[0]['ra']
+                dec = nf.iloc[0]['dec']
+                t = nf.iloc[0]['t']
+
+                # adding edge points
+                edge_points = pd.DataFrame({
+                    'col': [0, 0, 800, 800],
+                    'row': [0, 800, 0, 800],
+                    'ra': [ra,ra,ra,ra],
+                    'dec': [dec,dec,dec,dec]
+                    't': [t,t,t,t]
+                })
+
+                # Append the new rows to the DataFrame
+                nf = nf.append(edge_points, ignore_index=True)
+
                 print("quickbinning")
-                # std dev of ra and dec by col and row
                 ra_stdev = bin2d(nf['col'], nf['row'], nf['ra'], 'std', nbins)
                 dec_stdev = bin2d(nf['col'], nf['row'], nf['dec'], 'std', nbins)
-
-                # photon count per bin
                 count = bin2d(nf['col'], nf['row'], nf['ra'], 'count', nbins)
                 count = count / expt
 
                 print("masking binned data")
-                # density mask
                 density_mask = count >= .9
-                # dispersion mask
                 disp_mask = ra_stdev + dec_stdev > .014
-                # response map
-                dark_mask = count <= .015
+                dark_mask = count <= .008
 
                 print("making new masks")
-                # empty masks
                 hmask = np.ones(count.shape, dtype=bool)
                 cmask = np.ones(count.shape, dtype=bool)
 
-                # saving hotspot mask per eclipse and band
                 hmask[density_mask & disp_mask] = 0
                 print("saving hotspot mask")
                 hmask.tofile(f'{savepath}{eclipse}-{band}d-hmask.bin')
 
-                # saving coldspot mask per eclipse and band
                 cmask[dark_mask] = 0
                 print("saving coldspot mask")
                 cmask.tofile(f'{savepath}{eclipse}-{band}d-cmask.bin')
