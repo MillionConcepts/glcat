3d6af89db988001c313636454b981a266be8af4a
Zack Weinberg <zack@owlfolio.org>
2025-08-15 12:04:02 -0400

    hopefully final version of boresight_from_scst.py

--- notebooks_and_retired_scripts/cache_scst_and_urls.py
+++ notebooks_and_retired_scripts/cache_scst_and_urls.py
@@ -10,9 +10,11 @@ from pathlib import Path
 from typing import Iterable
 
 from gPhoton.io import mast
+from astropy.io import fits
 from limiter import Limiter
 from pyarrow import parquet, Table, Array, ChunkedArray
 
+DEFAULT_MAX_ECLIPSE = 50000
 
 START_TIME = None
 def progress(msg: str) -> None:
@@ -41,7 +43,25 @@ def retrieve_urls_and_scst(
 ) -> MastMetadata:
     with rate_limit:
         paths = mast.get_raw_paths(eclipse)
-        scst_file = mast.download_data(eclipse, ftype='scst', datadir=scst_dir)
+        if paths.get("scst") is None:
+            scst_file = None
+        else:
+            scst_file = mast.download_data(
+                eclipse, ftype='scst', datadir=scst_dir
+            )
+            try:
+                with fits.open(scst_file, lazy_load_hdus=False) as hdulist:
+                    pass
+            except Exception:
+                # the scst file is corrupt on MAST, rename it out of
+                # the way and mark this eclipse as not having an scst file
+                scst_file.rename(
+                    scst_file.with_name(
+                        scst_file.name + ".corrupt"
+                    )
+                )
+                scst_file = None
+
         return MastMetadata(
             eclipse = eclipse,
             scst_file = scst_file,
@@ -53,39 +73,73 @@ def retrieve_urls_and_scst(
 def eclipses_to_do(
     datadir: Path,
     shelf: shelve.Shelf,
-    metadata: Table
+    max_eclipse: int,
+    eclipses_with_aspect: frozenset,
 ) -> set[int]:
-    eclipses = set(e.as_py() for e in metadata.column('eclipse'))
-    prune = []
-    for e, mm in shelf.items():
-        try:
+
+    todo = set()
+    for e in range(max_eclipse):
+        se = str(e)
+        if se not in shelf:
+            todo.add(e)
+            continue
+
+        mm = shelf[se]
+        if (
+                "scst" not in mm
+                or "nuv_raw6" not in mm
+                or "fuv_raw6" not in mm
+        ):
+            # there's something wrong with the record in the shelf
+            scst_1 = f"e{e:05}-scst.fits.gz"
+            scst_2 = mm.get("scst")
+            (datadir / scst_1).unlink()
+            if scst_2 != scst_1 and scst_2 is not None:
+                (datadir / scst_2).unlink()
+            del shelf[se]
+            todo.add(e)
+            continue
+
+        if mm["scst"] is not None:
             scst = datadir / mm["scst"]
-            st = scst.stat()
-            if st.st_size > 0:
-                eclipses.remove(int(e))
+            try:
+                st = scst.stat()
+            except FileNotFoundError:
+                del shelf[se]
+                todo.add(e)
+                continue
+            if st.st_size == 0:
+                # the scst file exists but is truncated;
+                # delete it so mast.download_data will retry
+                scst.unlink()
+                del shelf[se]
+                todo.add(e)
                 continue
 
-            # if we reach this point, the scst file exists but is truncated;
-            # delete it so mast.download_data will retry
-            scst.unlink()
-        except (KeyError, FileNotFoundError):
-            pass
+            try:
+                with fits.open(scst, lazy_load_hdus=False) as hdulist:
+                    pass
+            except Exception:
+                # the scst file is corrupt, delete it so mast.download_data
+                # will retry
+                scst.unlink()
+                del shelf[se]
+                todo.add(e)
+                continue
 
-        # if we reach this point, there's something wrong with the
-        # record in the shelf, and we should regenerate it
-        prune.append(e)
+        if "gphoton_aspect" not in mm:
+            mm["gphoton_aspect"] = (e in eclipses_with_aspect)
+            shelf[se] = mm
 
-    for e in prune:
-        del shelf[e]
-
-    return eclipses
+    return todo
 
 
 def process_eclipses(
     datadir: Path,
-    metadata: Table,
     average_rate: int,
     burst_rate: int,
+    max_eclipse: int,
+    eclipses_with_aspect: frozenset
 ) -> None:
 
     progress("counting eclipses...")
@@ -94,7 +148,12 @@ def process_eclipses(
 
     with ThreadPoolExecutor() as pool, \
          shelve.open(datadir / "index.shelf") as shelf:
-        eclipses = eclipses_to_do(datadir, shelf, metadata)
+        eclipses = eclipses_to_do(
+            datadir,
+            shelf,
+            max_eclipse,
+            eclipses_with_aspect,
+        )
 
         progress("queuing...")
         futures = []
@@ -117,10 +176,15 @@ def process_eclipses(
                 progress(f"{completed}/{len(futures)} eclipses queried, {errors} errors")
             try:
                 mm = fut.result()
+                if mm.scst_file is None:
+                    scst = None
+                else:
+                    scst = str(mm.scst_file.relative_to(datadir))
                 shelf[str(mm.eclipse)] = {
-                    "scst": str(mm.scst_file.relative_to(datadir)),
+                    "scst": scst,
                     "nuv_raw6": mm.nuv_raw6,
-                    "fuv_raw6": mm.fuv_raw6
+                    "fuv_raw6": mm.fuv_raw6,
+                    "gphoton_aspect": (mm.eclipse in eclipses_with_aspect),
                 }
             except Exception as e:
                 t = type(e)
@@ -138,23 +202,35 @@ def process_eclipses(
 
 def main():
     ap = argparse.ArgumentParser()
-    ap.add_argument("metadata", type=Path,
-                    help="Parquet file containing eclipse metadata")
+    ap.add_argument("aspect", type=Path,
+                    help="Parquet file containing eclipse aspect data")
     ap.add_argument("data_dir", type=Path,
                     help="Where to write SCST files and index")
+    ap.add_argument("--max-eclipse", type=int, default=DEFAULT_MAX_ECLIPSE,
+                    help="Maximum eclipse number to query")
     ap.add_argument("--avg-rate", type=int, default=500,
                     help="Average number of MAST queries per second")
     ap.add_argument("--burst-rate", type=int, default=1000,
                     help="Burst rate (absolutely no more than this many queries per second)")
     args = ap.parse_args()
 
-    progress("loading metadata table...")
-    md = parquet.read_table(args.metadata)
-    if "eclipse" not in md.column_names:
+    progress("loading aspect table...")
+    aspect = parquet.read_table(args.aspect)
+    if "eclipse" not in aspect.column_names:
         sys.stderr.write(f"{args.metadata}: error: no 'eclipse' column found")
         sys.exit(1)
 
-    process_eclipses(args.data_dir, md, args.avg_rate, args.burst_rate)
+    eclipses_with_aspect = frozenset(
+        aspect.column("eclipse").to_pylist()
+    )
+
+    process_eclipses(
+        args.data_dir,
+        args.avg_rate,
+        args.burst_rate,
+        args.max_eclipse,
+        eclipses_with_aspect,
+    )
 
 
 main()
